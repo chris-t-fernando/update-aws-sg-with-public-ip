afaccc72ca35b1b1be99cf1bd0a0a51785a40986
Chris Fernando <c.fernando@optusnet.com.au>
2021-09-02 12:37:18 +0000

    revised and improved - deletes old rules, replaces with new IP. Uses tags to find old entries. Future improvement: store old IP in SSM and look for that instead of tags, but whatever

--- app.py
+++ app.py
@@ -5,48 +5,66 @@ ip = get("https://api.ipify.org").text + "/32"
 
 ec2Client = boto3.client("ec2")
 
-try:
-    response = ec2Client.authorize_security_group_ingress(
-        GroupId="sg-8b5c50ee",
-        IpPermissions=[
-            {
-                "FromPort": 3306,
-                "ToPort": 3306,
-                "IpProtocol": "tcp",
-                "IpRanges": [
+describe_response = ec2Client.describe_security_group_rules(
+    Filters=[
+        {
+            "Name": "group-id",
+            "Values": [
+                "sg-8b5c50ee",
+            ],
+        },
+    ],
+)
+
+
+for r in describe_response["SecurityGroupRules"]:
+    stale_rule = False
+    for t in r["Tags"]:
+        if t["Key"] == "home-ip" and t["Value"].lower() == "true":
+            # found an existing SG entry. Now check if it is for my IP
+            if r["CidrIpv4"] != ip:
+                # my IP has changed since last run - need to delete this rule and re-create it
+                stale_rule = True
+                print(f"Found stale rule - {r['CidrIpv4']}")
+                break
+
+    if stale_rule:
+        # delete the old rule
+        try:
+            delete_response = ec2Client.revoke_security_group_ingress(
+                GroupId="sg-8b5c50ee",
+                SecurityGroupRuleIds=[
+                    r["SecurityGroupRuleId"],
+                ],
+            )
+            print(f"Successfully deleted stale rule ID {r['SecurityGroupRuleId']}")
+        except Exception as e:
+            print(
+                f"Failed to delete stale rule ID {r['SecurityGroupRuleId']}.  Error: {str(e)}"
+            )
+            exit()
+
+        # create the new rule
+        try:
+            create_response = ec2Client.authorize_security_group_ingress(
+                GroupId="sg-8b5c50ee",
+                IpPermissions=[
                     {
-                        "CidrIp": ip,
-                        "Description": "SSH access from home",
+                        "FromPort": r["FromPort"],
+                        "ToPort": r["ToPort"],
+                        "IpProtocol": "tcp",
+                        "IpRanges": [
+                            {
+                                "CidrIp": ip,
+                                "Description": "Access from home network",
+                            },
+                        ],
                     },
                 ],
-            },
-        ],
-    )
-    print(f"Set 3306 rule succesfully.")
+            )
+            print(f"Set {r['FromPort']} rule succesfully.")
 
-except Exception as e:
-    if e.response["Error"]["Code"] != "InvalidPermission.Duplicate":
-        print(f"Unable to set 3306 rule. Error: {str(e)}")
-
-try:
-    response = ec2Client.authorize_security_group_ingress(
-        GroupId="sg-8b5c50ee",
-        IpPermissions=[
-            {
-                "FromPort": 22,
-                "ToPort": 22,
-                "IpProtocol": "tcp",
-                "IpRanges": [
-                    {
-                        "CidrIp": ip,
-                        "Description": "SSH access from home",
-                    },
-                ],
-            },
-        ],
-    )
-
-    print(f"Set 22 rule succesfully.")
-except Exception as e:
-    if e.response["Error"]["Code"] != "InvalidPermission.Duplicate":
-        print(f"Unable to set 22 rule. Error: {str(e)}")
+        except Exception as e:
+            print("error")
+            if e.response["Error"]["Code"] != "InvalidPermission.Duplicate":
+                print(f"Unable to set {r['FromPort']} rule. Error: {str(e)}")
